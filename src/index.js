6f5d207095f589c0c1e529fcec5c5352176e67e3
Michal <76213511+yvesrilla@users.noreply.github.com>
2023-04-12 23:30:57 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const {config} = from('dotenv')
 const {Client} = from('discord.js');
+
+config();
 
 const client = new Client({intents: ['Guilds', 'GuildMessages']})
 
-const TOKEN = 
-    'ODE5NTE5NzU0Nzc4NzA1OTcw.Gqzemn.FqZpljp7p-20FET1LLSd0EXnJa4mGjdLqF3xFI'
 
-client.login(TOKEN)
+
+client.login(process.env.BOT_TOKEN)
