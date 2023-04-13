3a612a91e7639ba1e1c1d5b0cb7d71f9be11c5e8
Michal <76213511+yvesrilla@users.noreply.github.com>
2023-04-13 23:49:13 +0000

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const {config} = require('dotenv')
-const {Client} = require('discord.js');
+const {Client, ActivityType} = require('discord.js');
 
 config();
 
 const client = new Client({intents: ['Guilds', 'GuildMessages']})
 
-
+client.on('ready', () =>
+{
+    console.log("online!");
+    client.user.setActivity('my dev rewrite code!', { type: ActivityType.Watching });
+});
 
 client.login(process.env.BOT_TOKEN)
